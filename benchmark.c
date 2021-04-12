4c3e344a3054fece4a677302cb6a51d1dfc6320c
kxdc <kvuexlu@gmail.com>
2021-04-13 05:19:15 +0800

    Prompt usage and parse input params

--- benchmark.c
+++ benchmark.c
@@ -2,4 +2,15 @@
 
 int main(int argc, char* argv[])
 {
+    if(argc != 3)
+    {
+        printf("Usage: %s <SIZE> <STRIDE>\n",argv[0]);
+        printf("Wallk an allcated memory buffer, generating read throughput results.\n");
+        printf("  <SIZE> is the byte size of buffer.\n");
+        printf("  <STRIDE> is the distance of two successive reads (in 64bit items).\n");
+        exit(1);
+    }
+
+    size = atoll(argv[1]);
+    stride = atoll(argv[2]);
 }
